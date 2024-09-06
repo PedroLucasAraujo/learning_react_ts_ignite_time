b61e3de4db0f08335c93fb0827cd6b022b388b4a
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-06 16:50:44 -0300

    feat: styled components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 interface ButtonContainerProps {
@@ -17,6 +17,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   height: 40px;
 
   ${(props) => {
-    return `background-color: ${buttonVariants[props.variant]}`;
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
   }};
 `;
