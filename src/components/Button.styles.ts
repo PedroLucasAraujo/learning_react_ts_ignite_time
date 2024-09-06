67776dff913f254dfd9116bbb84c900231235954
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-06 16:57:35 -0300

    feat: setting up themes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -15,10 +15,11 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  cursor: pointer;
+  margin: 8px;
 
-  ${(props) => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `;
-  }};
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
 `;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  white: "ffffff",
+  primary: "#8257e6",
+  secondary: "orange",
+  success: "green",
+  danger: "red",
+};
