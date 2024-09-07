05da0d5c4580e88701702d45623a844889b98b94
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-07 13:43:07 -0300

    feat: integrate form with typescript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
 import { Play } from "phosphor-react";
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
 import { useForm } from "react-hook-form"
+import { zodResolver} from "@hookform/resolvers/zod"
+import * as zod from "zod"
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod.number().min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+    .max(60, "O ciclo precisa ser de no máximo 5 minutos"),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+        resolver: zodResolver(
+            newCycleFormValidationSchema
+        ),
+        defaultValues:{
+            task: "",
+            minutesAmount: 0
+        }
+    })
 
     function handleCreateNewCycle(data: any) {
         console.log(data)
