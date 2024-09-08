69c9a672eb11ce8ae6530ecd013db9abce59d9fb
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-08 14:21:53 -0300

    feat: reducing countdown as seconds goes by

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,7 +11,8 @@ import {
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -27,6 +28,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -42,12 +44,25 @@ export function Home() {
     },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+
   function handleCreateNewCycle(data: any) {
     const id = String(new Date().getTime());
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
@@ -57,8 +72,6 @@ export function Home() {
     console.log(data);
     reset();
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const remainingSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
@@ -111,9 +124,7 @@ export function Home() {
         <CountDownContainer>
           <span>{minutes[0]}</span>
           <span>{minutes[1]}</span>
-
           <Separator>:</Separator>
-
           <span>{seconds[0]}</span>
           <span>{seconds[1]}</span>
         </CountDownContainer>
