65df2c33af434b2b3e160e9b8a86656720dde6b5
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-08 14:33:26 -0300

    feat: countdown bugs fix, and countdown on page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,13 +47,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   function handleCreateNewCycle(data: any) {
@@ -67,6 +72,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
     alert("Ciclo criado!");
 
     console.log(data);
@@ -81,6 +87,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds]);
 
   const task = watch("task");
   const isSubmitDisabled = !task;
