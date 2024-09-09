e3c6f4a1834e94fa1021618b47606349760f3b05
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-09 18:49:06 -0300

    feat: create cycles context and refact home components code with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom"
- 
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
-     
+
       <GlobalStyle />
     </ThemeProvider>
   );
