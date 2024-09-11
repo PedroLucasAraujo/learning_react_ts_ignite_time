0ab81553e8dbd259f84838ebd67189a769b39d3e
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-11 18:13:45 -0300

    fix: adjust historic list

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,14 +1,15 @@
 import { useContext } from "react";
-import { HistoryContainer, HistoryList, Status } from "./styles";
-import { CyclesContext } from "../../contexts/CyclesContext";
 import { formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { CyclesContext } from "../../contexts/CyclesContext";
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
   const { cycles } = useContext(CyclesContext);
 
   return (
     <HistoryContainer>
-      <h1>Meu Histórico</h1>
+      <h1>Meu histórico</h1>
 
       <HistoryList>
         <table>
@@ -16,7 +17,7 @@ export function History() {
             <tr>
               <th>Tarefa</th>
               <th>Duração</th>
-              <th>Início</th>
+              <th>Duração</th>
               <th>Status</th>
             </tr>
           </thead>
@@ -24,11 +25,11 @@ export function History() {
             {cycles.map((cycle) => {
               return (
                 <tr key={cycle.id}>
-                  <td>{cycle.task} </td>
+                  <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
-                      addSuffix: true, // adiciona o sufixo de 'agora'
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
                       locale: ptBR,
                     })}
                   </td>
@@ -41,7 +42,7 @@ export function History() {
                       <Status statusColor="red">Interrompido</Status>
                     )}
 
-                    {!cycle.interruptedDate && !cycle.finishedDate && (
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
                   </td>
